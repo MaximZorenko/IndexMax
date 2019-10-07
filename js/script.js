136c1ea3cfa3be583f5ff6edea28ac1af3176fad
MaximZorenko <zorenko1990@gmail.com>
2019-10-08 01:53:16 +0300

    Lucid - added popap

--- js/script.js
+++ js/script.js
@@ -10,6 +10,35 @@
 	}
 })();
 
+////////////////////////////open popap
+
+(()=>{
+	let cross = document.querySelector('.cross'),
+			popap = document.querySelector('.lucid-popap'),
+			button = document.querySelectorAll('.btn-b'),
+			open = ()=>{
+				popap.classList.add('open');
+			},
+			closs = ()=>{
+				popap.classList.remove('open');
+			};
+
+	for(let i = 0; i < button.length; i++){
+			button[i].addEventListener('click',open);
+	}
+
+	cross.addEventListener('click',closs);
+})();
+
+
+
+
+////////////////////////////////////
+
+
+
+/////////////////////////////////////
 $('.carousel').carousel({
   interval: false
 })
+
